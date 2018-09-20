fd25f1ef3bd8109a6f5ae542226e97aa5d9091c7
XinAn <xinan@1deMacBook-Air.local>
2018-09-20 21:51:12 +0800

    fix:增加设计模式笔记

---
+++ design-pattern-test/src/index9.js
@@ -0,0 +1,37 @@
+class LoginForm {
+  constructor () {
+    this.state = 'hide'
+  }
+  show() {
+    if(this.state === 'show') {
+      alert('已经显示')
+      return
+    }
+    this.state = 'show'
+     console.log( '登录框显示成功');
+  }
+ hide() {
+    if(this.state === 'hide') {
+      alert('已经显示')
+      return
+    }
+   this.state = 'show'
+   console.log( '登录框隐藏成功');
+ }
+}
+LoginForm.getInstance = (function () {
+  let instance
+  return function () {
+    if(!instance) {
+      instance = new LoginForm()
+    }
+    return instance
+  }
+})()
+
+
+let login1 = LoginForm.getInstance()
+login1.show()
+
+let login2 = LoginForm.getInstance()
+login1.hide()
